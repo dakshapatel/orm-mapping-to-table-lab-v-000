0e34f14ab9b53b7ec6f02b849418fa175b432efc
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 02:25:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,29 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+
+  def attributes(name:, grade:)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table (name:, grade:)
+
+
+
+  end
+
+  def drop_table
+
+  end
+
+
+  def save
+
+  end
+
+  def
 end
