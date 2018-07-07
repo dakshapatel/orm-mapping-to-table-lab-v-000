6190b5c0a85aa2efdcc41f12f0d763bbe76252b9
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 02:29:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def attributes(name:, grade:)
+  def attributes(name:, grade:, id: = nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table 
